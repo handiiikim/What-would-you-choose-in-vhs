a25e6c4b25e57012eaac91179189426b983cb92c
handiiikim <darami95@gmail.com>
2020-05-17 19:40:44 +0200

    add

--- start/script/script3-3.js
+++ start/script/script3-3.js
@@ -12,10 +12,10 @@ setTimeout("showIt2()", 3000); // after 5 secs
 function showIt3() {
   document.getElementById("objector").style.visibility = "visible";
 }
-setTimeout("showIt3()", 9000); // after 5 secs
+setTimeout("showIt3()", 11000); // after 5 secs
 
 
 function showIt4() {
   document.getElementById("question1").style.visibility = "visible";
 }
-setTimeout("showIt4()", 14000); // after 5 secs
+setTimeout("showIt4()", 15000); // after 5 secs
